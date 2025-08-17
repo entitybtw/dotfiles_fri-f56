2ded8c988fba5e9da91a635cacfdb38dd0b1c3a8
entitybtw <studenov.ivan020312@gmail.com>
2025-08-17 07:20:51 +0000

    update term.sh

--- .config/rofi/scripts/term.sh
+++ .config/rofi/scripts/term.sh
@@ -13,13 +13,9 @@ arch() {
     kitty --hold -e strat -r arch zsh -c "neofetch --ascii_distro arch_small && exec zsh" &
 }
 
-void() {
-    kitty --hold -e strat -r void zsh -c "neofetch --ascii_distro void_small && exec zsh" &
-}
-
 # Menu
 main_menu() {
-    choice=$(echo -e "bedrock ->\ngentoo ->\narch ->\nvoid ->" | rofi -dmenu -p "|")
+    choice=$(echo -e "bedrock ->\ngentoo ->\narch ->" | rofi -dmenu -p "|")
     case "$choice" in
         "bedrock ->")
             bedrock
@@ -29,9 +25,6 @@ main_menu() {
             ;;
         "arch ->")
             arch
-            ;;
-        "void ->")
-            void
             ;;
         *) exit ;;  # Exit the script on any other choice
     esac
